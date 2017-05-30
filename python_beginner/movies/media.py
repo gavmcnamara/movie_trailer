6e66bab43a8a7a8678486fb91d614b828b687441
gavmcnamara <gavin.mcnamara01@myhunter.cuny.edu>
2017-05-29 22:27:18 -0400

    Update media.py

--- python_beginner/movies/media.py
+++ python_beginner/movies/media.py
@@ -7,12 +7,18 @@ class Movie():
     valid_ratings = ["G", "PG", "PG-13", "R"]
     def __init__(self, movie_title, movie_storyline, poster_image,
                           trailer_youtube):
+        
+        """ This function helps define all the instances we want to include for our movies"""
+        
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
+   
     def show_trailer(self):
+        
+        """ This function allows for the movie trailer to be played"""
+        
         webbrowser.open(self.trailer_youtube_url)
         
 
